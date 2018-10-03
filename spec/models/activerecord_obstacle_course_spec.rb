1498992537d16a2261145d2b18f19cb611637820
Cesar Jolibois <diazcd85@gmail.com>
2018-10-02 18:44:49 -0600

    2nd test pass

--- spec/models/activerecord_obstacle_course_spec.rb
+++ spec/models/activerecord_obstacle_course_spec.rb
@@ -57,7 +57,7 @@ describe 'ActiveRecord Obstacle Course' do
     # ------------------------------------------------------------
 
     # ------------------ Using ActiveRecord ----------------------
-    # Solution goes here
+    order_id = Order.minimum('id')
     # ------------------------------------------------------------
 
     # Expectation
